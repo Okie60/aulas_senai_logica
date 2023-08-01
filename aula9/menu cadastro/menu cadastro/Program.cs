890b2534ff1f95bf2b79e7969629d2f492b0c5ba
OkieDokie <87245056+Okie60@users.noreply.github.com>
2023-08-01 10:30:25 -0300

    Add files via upload

---
+++ aula9/menu cadastro/menu cadastro/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using static System.Console;
+
+namespace uauLegal//recomendado usar o nome do projeto
+{
+    class MainClass
+    {
+        public static void printmenu(string[] options)
+        {
+            foreach (String option in options)//listando o array String[]
+            {
+                WriteLine(option);
+            }
+            WriteLine("Escolha sua opção:");
+        }
+
+        public static void Main(String[] args)
+        {
+            WriteLine("Cadastro de pessoas");//menu
+            String[] options =
+            {
+                "1 - Cadastrar",
+                "2 - Editar",
+                "3 - Excluir",
+                "4 - Listar",
+                "5 - Sair"
+            };
+            int option = 0;
+            while (true)
+            {
+                printmenu(options);//começo da escolha de menu
+                try
+                {
+                    option = Convert.ToInt32(ReadLine());
+                }
+                catch (System.FormatException)//lidando com a teimosia do usuário
+                {
+                    WriteLine("Digite uma opção entre 1 e " + options.Length);
+                    continue;
+                }
+                catch (Exception)//lidando com erros
+                {
+                    WriteLine("Ocorreu um erro! :(");
+                    continue;
+                }
+                switch (option)//escolha de opções
+                {
+                    case 1:
+                        Cadastrar();
+                        break;
+                    case 2:
+                        Editar();
+                        break;
+                    case 3:
+                        Excluir();
+                        break;
+                    case 4:
+                        Listar();
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        WriteLine("Escolha uma opção entre 1 e " + options.Length);
+                        break;
+                }
+            }
+        }
+
+        static List<string> nomes = new List<string>();
+        static List<string> cidade = new List<string>();
+        static List<int> cpf = new List<int>();
+
+        private static void Cadastrar()
+        {
+            Clear();
+            WriteLine("Cadastro de Pessoa\n");
+            WriteLine("Digite o nome da pessoa:");
+            nomes.Add(ReadLine());//adiciona nomes a uma posição da lista
+            WriteLine("Digite a cidade de residência:");
+            cidade.Add(ReadLine());
+            WriteLine("Digite o CPF:");
+            cpf.Add(Convert.ToInt32(ReadLine()));//mesma coisa com números
+            WriteLine();
+        }
+
+        private static void Editar()
+        {
+            Clear();
+            WriteLine("Edite seu cadastro\n");
+            string nome = "";
+            WriteLine("Digite o nome do usuário");
+            nome = ReadLine();
+            int index = nomes.IndexOf(nome);//procura um nome pela posição
+            if (index >= 0)
+            {
+                WriteLine("Registro");
+                WriteLine($"Nome: {nomes[index]} - Cidade: {cidade[index]} - CPF: {cpf[index]}");
+
+                WriteLine("\nDigite um novo nome:");
+                nomes[index] = ReadLine();
+                WriteLine("\nDigite a nova cidade:");
+                cidade[index] = ReadLine();
+                WriteLine("\nDigite o novo CPF:");
+                cpf[index] = Convert.ToInt32(ReadLine());
+            }
+            else
+            {
+                WriteLine("Registro não encontrado!!!");
+            }
+
+        }
+
+        private static void Excluir()
+        {
+            Clear();
+            WriteLine("Exclua seu cadastro\n");
+            string nome = "";
+            WriteLine("Digite o nome do usuário");
+            nome = ReadLine();
+            int index = nomes.IndexOf(nome);//procura um nome pela posição
+            if (index >= 0)
+            {
+                WriteLine("Registro Removido");
+                WriteLine($"Nome: {nomes[index]} - Cidade: {cidade[index]} - CPF: {cpf[index]}");
+
+                nomes.RemoveAt(index);
+                cidade.RemoveAt(index);
+                cpf.RemoveAt(index);
+                WriteLine("Registro removido com sucesso!\n");
+            }
+            else
+            {
+                WriteLine("Registro não encontrado!!!");
+            }
+        }
+        private static void Listar()
+        {
+            Clear();
+            WriteLine("Listagem");
+            int pos = 0;
+            foreach (var item in nomes)
+            {
+                WriteLine($"Nome: {item} - Cidade: {cidade[pos]} - CPF: {cpf[pos]}");
+                pos++;
+            }
+        }
+    }
+}
